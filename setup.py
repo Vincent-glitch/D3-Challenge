81cc272d64f4dcbf64cb4f4a22e2228584850ad3
Vincent-glitch <66323103+Vincent-glitch@users.noreply.github.com>
2020-12-04 14:31:14 -0500

    cleaner

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analyzing trends in census data to create visualizations for stories about health risks vs particular demographics.',
+    author='Vincent D'Alto',
+    license='MIT',
+)
